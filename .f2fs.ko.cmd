a7a1b13256d10f1306ecf92e345c8f70f39951b3
erikliu123 <1171045702@qq.com>
2019-03-17 13:29:24 +0800

    remove useless files

--- .f2fs.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/peter/Desktop/f2fs/f2fs.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/peter/Desktop/f2fs/f2fs.ko /home/peter/Desktop/f2fs/f2fs.o /home/peter/Desktop/f2fs/f2fs.mod.o ;  true
--- force_rmmod/.tmp_versions/force_rmmod.mod
+++
@@ -1,3 +0,0 @@
-/home/peter/Desktop/f2fs/force_rmmod/force_rmmod.ko
-/home/peter/Desktop/f2fs/force_rmmod/force_rmmod.o
-
